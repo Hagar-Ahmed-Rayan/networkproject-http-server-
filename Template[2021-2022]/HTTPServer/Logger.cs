4f8c82ced2e88854f9cdf065e68f7257d9083644
hagar <73147463+gogyryan@users.noreply.github.com>
2022-02-05 02:37:04 +0200

    Add files via upload

---
+++ Template[2021-2022]/HTTPServer/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HTTPServer
+{
+    class Logger
+    {
+        //static StreamWriter sr = new StreamWriter("log.txt");
+        public static void LogException(Exception ex)
+        {
+            // TODO: Create log file named log.txt to log exception details in it
+            //FileStream fs = new FileStream(@"D:\STUDY\networks\Template[2021-2022]
+            // \HTTPServer\bin\Debug\log.txt",   FileMode.OpenOrCreate);
+            //Datetime:
+            //message:
+            // for each exception write its details associated with datetime 
+            StreamWriter sr = new StreamWriter("log.txt");
+            sr.WriteLine("date time: " + DateTime.Now.ToString());
+            sr.WriteLine("massege: " + ex.Message);
+
+            sr.Close();
+        }
+    }
+}
